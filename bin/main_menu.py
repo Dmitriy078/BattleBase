35e9bd5fcc8fc4a23856290ccc3fab2254bfbe49
Dmitriy078 <dima.o.ryabov@gmail.com>
2024-12-03 19:02:23 +0300

    update main_menu and gitignore

--- bin/main_menu.py
+++ bin/main_menu.py
@@ -1,7 +1,12 @@
+import os
 class MainMenu:
     def __init__(self, settings):
         self.settings = settings
         self.is_reboot = False
+
+
+        if not os.path.exists("data"):
+            os.makedirs("data")
 
     def show_menu(self):
         pass
